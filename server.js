5d621384fd49432578a4556288f11b1a8d18fd8d
David Aylward <davidaylward@Davids-Mac-mini.lan>
2022-03-24 17:50:40 -0400

    fixed mistake in server js. added appuse html

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended: true}));
 
-app.get('*',(req, res)=>{
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-})
+app.use('/', htmlRoutes)
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
