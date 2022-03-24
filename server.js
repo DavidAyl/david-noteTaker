1b1e79c627285e74ff03b28fc5210b8bac8e68ee
David Aylward <davidaylward@Davids-Mac-mini.lan>
2022-03-24 17:24:57 -0400

    added routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
