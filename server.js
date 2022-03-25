fa5e276dd576343401e96ce1dd68ee6869ded9e1
David Aylward <davidaylward@Davids-Mac-mini.lan>
2022-03-25 16:54:43 -0400

    refactored the code into a single html document, also adde async writing and reading

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+// Brining in things needed for code
 const express = require('express');
 const path = require('path');
-const apis = require('./routes/apiRoutes')
 const util = require('util');
 const fs = require('fs');
 const res = require('express/lib/response');
 
+// Allows me to read and write files asynchronously
 const readAsync = util.promisify(fs.readFile);
 const writeAsync = util.promisify(fs.writeFile);
 
+// Which port it will run on with express
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -17,13 +19,15 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended: true}));
 
-app.get('./api/notes', function(req, res) {
+// Fetches notes
+app.get('/api/notes', function(req, res) {
   readAsync('./db/db.json', 'utf8').then(function(data) {
     notes = [].concat(JSON.parse(data))
     res.json(notes)
   })
 })
 
+// Saves new notes to db.json
 app.post("/api/notes", function(req, res) {
   const note = req.body;
   readAsync('./db/db.json', 'utf8').then(function(data) {
@@ -44,12 +48,13 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
+// any other parameters or queries will bring back to index page
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 
-
+// Lets you know app is running on the port you choose
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
