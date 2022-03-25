10095b6d9060385bcd3ecae24437aa1235c14f5c
David Aylward <davidaylward@Davids-Mac-mini.lan>
2022-03-25 15:49:03 -0400

    updated apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const res = require('express/lib/response');
 let router = express.Router();
 const path = require('path');
+
+router.get('/notes', (req, res), function() {
+getnotes()
+.then((notes) => {
+    return res.json(notes);
+})
+.catch((err) => res.status(500).json(err));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const apiRoutes = require('./routes/apiRoutes')
-const htmlRoutes = require('./routes/htmlRoutes')
+const apis = require('./routes/apiRoutes')
+const htmls = require('./routes/htmlRoutes')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +12,8 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended: true}));
 
-app.use('/', htmlRoutes)
+app.use('/api', apis);
+app.use('/', htmls);
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
