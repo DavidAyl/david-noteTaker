1986f7456383dde503cc11d3f94aa4d69ea89ccc
David Aylward <davidaylward@Davids-Mac-mini.lan>
2022-03-25 15:57:00 -0400

    new commit

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,12 +3,12 @@ const res = require('express/lib/response');
 let router = express.Router();
 const path = require('path');
 
-router.get('/notes', (req, res), function() {
+router.get('/notes', ((req, res) => {
 getnotes()
 .then((notes) => {
     return res.json(notes);
 })
 .catch((err) => res.status(500).json(err));
-});
+}));
 
 module.exports = router;
